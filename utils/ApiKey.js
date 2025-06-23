2bdf212742cc8a10ce6238a7664be78b9afdf2eb
Raphael Tuor <raphael.tuor@gmail.com>
2025-06-23 16:58:57 +0200

    Added openAI API key to .env

--- utils/ApiKey.js
+++ utils/ApiKey.js
@@ -1,7 +1,6 @@
-export const newsApiKey = "7d5e14b51cb44a98b4357b6ff065b31d";
-//export const theNewsApiKey = "Ug5MEUSOHcG2UiEv0KmHeuOFghiEFV71uMsFyzb3";
-// backup key for theNewsApi:
-// raphael.tuor@gmail.com:
-//export const theNewsApiKey = "voTZso08VDUZDYIK4u2uV1Ow6B6UoYsAxtEbFlk0";
-// unifr:
-export const theNewsApiKey = "lFhwa82byO5DBs7erPiGPn587CwV8ZjwscrxYwEt";
+
+
+//// NEW CODE: 
+export const newsApiKey = process.env.EXPO_PUBLIC_NEWS_CATCHER_API_KEY;
+export const aptabaseApiKey = process.env.EXPO_PUBLIC_APTABASE_API_KEY;
+export const openAIApiKey = process
